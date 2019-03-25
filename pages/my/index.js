c63e1dacfddaa37ebd73097e8ea3f868573da916
zhoudaxiaa <1250198256@qq.com>
2019-03-25 21:55:58 +0800

    写我的

--- pages/my/index.js
+++ pages/my/index.js
@@ -5,62 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
-    bgImg: ''
+    bgImg: 'https://static.sesine.com/wechat-weapp-movie//images/user_bg_1.jpg',
+    isLogin: false,
+    avatar: '',
+    name: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+                // 可以将 res 发送给后台解码出 unionId
+                this.setData({
+                  avatar: res.userInfo.avatarUrl,
+                  name: res.userInfo.nickName,
+                  isLogin: true
+                })
+            }
+          }, 3000);
+ 
+        } else {
+          this.setData({
+            isLogin: false
+          })
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getUserInfo(e) {
+    this.setData({
+      avatar: e.detail.userInfo.avatarUrl,
+      name: e.detail.userInfo.nickName,
+      isLogin: true
+    })
   }
+
 })
